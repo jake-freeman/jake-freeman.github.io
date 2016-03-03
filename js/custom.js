47f343335ee3165a7eab4b235795f6a604de0359
jake-freeman <jfreeman@hawk.iit.edu>
2016-03-03 14:58:55 -0600

    Add scripting to populate scores

--- js/custom.js
+++ js/custom.js
@@ -10,3 +10,21 @@ $('#nav').affix({
         top: $('#topnav').offset().top
     }
 });
+
+// fill circle spans
+var spans = $(".score");
+var span;
+spans.each(function(i, obj) {
+  span = $(this);
+  console.log("f");
+
+  var innerHTML = "";
+  for (var j = 0; j < 5; j++) {
+    innerHTML += "\n<span class=\"full-circle";
+    if (j < parseInt(span.data("value"))) {
+      innerHTML += " full";
+    }
+    innerHTML += "\"></span>"
+  }
+  span.append(innerHTML);
+});
